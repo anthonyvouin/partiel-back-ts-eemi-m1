3e2f26ae0bb145405d02df44547a7b71c2fae6e1
anthonyvouin <72597812+anthonyvouin@users.noreply.github.com>
2024-07-04 22:09:54 +0200

    add seed

---
+++ src/services/seed.ts
@@ -0,0 +1,89 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+import { ContactDocument, Contact } from "../models/contact.model";
+import { EventDocument, Event } from "../models/event.model";
+import { User, UserDocument } from "../models/user.model";
+import connectDB
+    from "../../config/db.config";
+dotenv.config();
+
+const seedDatabase = async () => {
+  try {
+    await connectDB();
+
+    await Contact.deleteMany({});
+    await Event.deleteMany({});
+    await User.deleteMany({});
+
+    const contacts = [
+      {
+        lastName: "Doe",
+        firstName: "John",
+        email: "john.doe@example.com",
+        objet: "Question",
+        message: "How can I reset my password?",
+        date: new Date(),
+      },
+      {
+        lastName: "Smith",
+        firstName: "Jane",
+        email: "jane.smith@example.com",
+        objet: "Feedback",
+        message: "Great service!",
+        date: new Date(),
+      },
+    ];
+    await Contact.insertMany(contacts);
+
+    const events = [
+      {
+        name: "Summer Festival",
+        date: new Date("2024-07-20"),
+        location: "Central Park",
+        description: "A fun festival with music and food.",
+        capacity: 5000,
+        finished: false,
+      },
+      {
+        name: "Tech Conference",
+        date: new Date("2024-09-15"),
+        location: "Convention Center",
+        description: "A conference for tech enthusiasts.",
+        capacity: 2000,
+        finished: false,
+      },
+    ];
+    await Event.insertMany(events);
+
+    const users = [
+      {
+        nom: "Dupont",
+        prenom: "Pierre",
+        dateNaissance: new Date("1985-05-15"),
+        adresse: "123 Rue de Paris, Paris, France",
+        contactTelephonique: "0123456789",
+        email: "pierre.dupont@example.com",
+        responsableLegal: "Jean Dupont",
+      },
+      {
+        nom: "Martin",
+        prenom: "Lucie",
+        dateNaissance: new Date("1990-11-25"),
+        adresse: "456 Avenue des Champs, Lyon, France",
+        contactTelephonique: "0987654321",
+        email: "lucie.martin@example.com",
+        responsableLegal: "Marie Martin",
+      },
+    ];
+    await User.insertMany(users);
+
+    console.log("Database seeded successfully");
+  } catch (error) {
+    console.error("Error seeding database:")
+  } finally {
+    mongoose.connection.close();
+  }
+};
+
+seedDatabase();
